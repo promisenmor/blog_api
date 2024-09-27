e8b3e8b6e7869b22688dec00cdb7ed5bc3a6bbfb
Promise Nmor <nmorchibogu98@gmail.com>
2024-09-27 16:25:45 +0100

    made changes to urls.py file

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from .views import PostListCreate, PostDetail, UserReigistration, CommentListCre
 
 urlpatterns = [
     path('posts/', PostListCreate.as_view(), name='post-list-create'),
-    path('Posts/<int:pk>', PostDetail.as_view(), name='post-detail'),
+    path('posts/<int:pk>', PostDetail.as_view(), name='post-detail'),
     path('register/', UserReigistration.as_view(), name='user-registration'),
     path('posts/<int:post_id>/comments', CommentListCreate.as_view(), name='comment-list-create'),
     path('comments/<int:pk>/', CommentDetail.as_view(), name='commen-detail')
